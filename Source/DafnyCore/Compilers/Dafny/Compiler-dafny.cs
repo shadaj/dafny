967f92c48f41d23ce64a9b26cc083dab780a265f
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-08-15 16:05:06 -0400

    Support subset types in the Rust backend
    <small>By submitting this pull request, I confirm that my contribution is made under the terms of the [MIT
    license](https://github.com/dafny-lang/dafny/blob/master/LICENSE.txt).</small>

--- Source/DafnyCore/Compilers/Dafny/Compiler-dafny.cs
+++ Source/DafnyCore/Compilers/Dafny/Compiler-dafny.cs
@@ -58,8 +58,6 @@ namespace Microsoft.Dafny.Compilers {
     }
 
     public override IReadOnlySet<Feature> UnsupportedFeatures => new HashSet<Feature> {
-      Feature.UnboundedIntegers,
-      Feature.RealNumbers,
       Feature.Ordinals,
       Feature.FunctionValues,
       Feature.Iterators,
@@ -139,6 +137,24 @@ namespace Microsoft.Dafny.Compilers {
       return name;
     }
 
+    protected override ConcreteSyntaxTree EmitCoercionIfNecessary(Type from, Type to, IToken tok, ConcreteSyntaxTree wr) {
+      if (from.AsSubsetType == null && to.AsSubsetType != null) {
+        if (wr is BuilderSyntaxTree<ExprContainer> stmt) {
+          return new BuilderSyntaxTree<ExprContainer>(stmt.Builder.SubsetUpgrade(GenType(to)));
+        } else {
+          return base.EmitCoercionIfNecessary(from, to, tok, wr);
+        }
+      } else if (from.AsSubsetType != null && to.AsSubsetType == null) {
+        if (wr is BuilderSyntaxTree<ExprContainer> stmt) {
+          return new BuilderSyntaxTree<ExprContainer>(stmt.Builder.SubsetDowngrade());
+        } else {
+          return base.EmitCoercionIfNecessary(from, to, tok, wr);
+        }
+      } else {
+        return base.EmitCoercionIfNecessary(from, to, tok, wr);
+      }
+    }
+
     protected override IClassWriter CreateClass(string moduleName, string name, bool isExtern, string fullPrintName,
       List<TypeParameter> typeParameters, TopLevelDecl cls, List<Type> superClasses, IToken tok, ConcreteSyntaxTree wr) {
       if (currentBuilder is ClassContainer builder) {
@@ -198,14 +214,17 @@ namespace Microsoft.Dafny.Compilers {
 
     protected override IClassWriter DeclareNewtype(NewtypeDecl nt, ConcreteSyntaxTree wr) {
       if (currentBuilder is NewtypeContainer builder) {
+        List<DAST.Statement> witnessStmts = new();
         DAST.Expression witness = null;
         if (nt.WitnessKind == SubsetTypeDecl.WKind.Compiled) {
           var buf = new ExprBuffer(null);
-          EmitExpr(nt.Witness, false, new BuilderSyntaxTree<ExprContainer>(buf), null);
+          var statementBuf = new StatementBuffer();
+          EmitExpr(nt.Witness, false, new BuilderSyntaxTree<ExprContainer>(buf), new BuilderSyntaxTree<StatementContainer>(statementBuf));
           witness = buf.Finish();
+          witnessStmts = statementBuf.PopAll();
         }
 
-        return new ClassWriter(this, builder.Newtype(nt.GetCompileName(Options), GenType(nt.BaseType), witness));
+        return new ClassWriter(this, builder.Newtype(nt.GetCompileName(Options), new(), GenType(nt.BaseType), witnessStmts, witness));
       } else {
         throw new InvalidOperationException();
       }
@@ -213,7 +232,7 @@ namespace Microsoft.Dafny.Compilers {
 
     private DAST.Type GenType(Type typ) {
       // TODO(shadaj): this is leaking Rust types into the AST, but we do need native types
-      var xType = DatatypeWrapperEraser.SimplifyType(Options, typ);
+      var xType = DatatypeWrapperEraser.SimplifyType(Options, typ, true);
 
       if (xType is BoolType) {
         return (DAST.Type)DAST.Type.create_Primitive(DAST.Primitive.create_Bool());
@@ -264,8 +283,26 @@ namespace Microsoft.Dafny.Compilers {
     }
 
     protected override void DeclareSubsetType(SubsetTypeDecl sst, ConcreteSyntaxTree wr) {
-      // TODO(shadaj): currently ignores subset types because they appear in the prelude
-      // throw new NotImplementedException();
+      if (currentBuilder is NewtypeContainer builder) {
+        List<DAST.Statement> witnessStmts = new();
+        DAST.Expression witness = null;
+        if (sst.WitnessKind == SubsetTypeDecl.WKind.Compiled) {
+          var statementBuf = new StatementBuffer();
+          var buf = new ExprBuffer(null);
+          EmitExpr(sst.Witness, false, new BuilderSyntaxTree<ExprContainer>(buf), new BuilderSyntaxTree<StatementContainer>(statementBuf));
+          witness = buf.Finish();
+          witnessStmts = statementBuf.PopAll();
+        }
+
+        List<DAST.Type> typeParams = new();
+        foreach (var tp in sst.TypeArgs) {
+          typeParams.Add((DAST.Type)DAST.Type.create_TypeArg(Sequence<Rune>.UnicodeFromString(tp.Name)));
+        }
+
+        builder.Newtype(sst.GetCompileName(Options), typeParams, GenType(sst.Rhs), witnessStmts, witness).Finish();
+      } else {
+        throw new InvalidOperationException();
+      }
     }
 
     protected override void GetNativeInfo(NativeType.Selection sel, out string name, out string literalSuffix, out bool needsCastAfterArithmetic) {
@@ -421,7 +458,7 @@ namespace Microsoft.Dafny.Compilers {
     }
 
     protected override string TypeDescriptor(Type type, ConcreteSyntaxTree wr, IToken tok) {
-      type = DatatypeWrapperEraser.SimplifyType(Options, type, true);
+      type = DatatypeWrapperEraser.SimplifyType(Options, type);
       return type.ToString();
     }
 
@@ -930,11 +967,8 @@ namespace Microsoft.Dafny.Compilers {
             break;
         }
 
-        if (e.Type.AsNewtype != null) {
-          baseExpr = (DAST.Expression)DAST.Expression.create_NewtypeValue(
-            GenType(e.Type),
-            baseExpr
-          );
+        if (e.Type.AsNewtype != null || e.Type.AsSubsetType != null) {
+          baseExpr = (DAST.Expression)DAST.Expression.create_SubsetUpgrade(baseExpr, GenType(e.Type));
         }
 
         builder.Builder.AddExpr(baseExpr);
@@ -981,7 +1015,8 @@ namespace Microsoft.Dafny.Compilers {
     }
 
     private DAST.Type FullTypeNameAST(UserDefinedType udt, MemberDecl member = null) {
-      if (udt is ArrowType arrow) {
+      if (udt.IsArrowType) {
+        var arrow = udt.AsArrowType;
         return (DAST.Type)DAST.Type.create_Arrow(
           Sequence<DAST.Type>.FromArray(arrow.Args.Select(m => GenType(m)).ToArray()),
           GenType(arrow.Result)
@@ -1013,6 +1048,12 @@ namespace Microsoft.Dafny.Compilers {
     private DAST.Type TypeNameASTFromTopLevel(TopLevelDecl topLevel, List<Type> typeArgs) {
       var path = PathFromTopLevel(topLevel);
 
+      bool nonNull = false;
+      if (topLevel is NonNullTypeDecl non) {
+        nonNull = true;
+        topLevel = non.Rhs.AsTopLevelTypeWithMembers;
+      }
+
       ResolvedType resolvedType;
       if (topLevel is NewtypeDecl) {
         resolvedType = (DAST.ResolvedType)DAST.ResolvedType.create_Newtype();
@@ -1023,6 +1064,8 @@ namespace Microsoft.Dafny.Compilers {
       } else if (topLevel is ClassDecl) {
         // TODO(shadaj): have a separate type when we properly support classes
         resolvedType = (DAST.ResolvedType)DAST.ResolvedType.create_Datatype(path);
+      } else if (topLevel is SubsetTypeDecl) {
+        resolvedType = (DAST.ResolvedType)DAST.ResolvedType.create_Newtype();
       } else {
         throw new InvalidOperationException(topLevel.GetType().ToString());
       }
@@ -1060,7 +1103,7 @@ namespace Microsoft.Dafny.Compilers {
           // sometimes, we don't actually call EmitDatatypeValue
           currentBuilder = origBuilder;
         }
-      } else if (expr is BinaryExpr) {
+      } else if (expr is BinaryExpr bin) {
         var origBuilder = currentBuilder;
         base.EmitExpr(expr, inLetExprBody, actualWr, wStmts);
         currentBuilder = origBuilder;
@@ -1299,7 +1342,9 @@ namespace Microsoft.Dafny.Compilers {
           ));
         }
 
-        return new BuilderSyntaxTree<StatementContainer>(builder.Builder.Lambda(formals));
+        var retType = GenType(resultType);
+
+        return new BuilderSyntaxTree<StatementContainer>(builder.Builder.Lambda(formals, retType));
       } else {
         throw new InvalidOperationException();
       }
@@ -1482,7 +1527,7 @@ namespace Microsoft.Dafny.Compilers {
     }
 
     protected override void EmitTypeTest(string localName, Type fromType, Type toType, IToken tok, ConcreteSyntaxTree wr) {
-      throw new NotImplementedException();
+      throw new UnsupportedFeatureException(tok, Feature.TypeTests);
     }
 
     protected override void EmitCollectionDisplay(CollectionType ct, IToken tok, List<Expression> elements,
